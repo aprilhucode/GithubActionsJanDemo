ca82eca0ade2e3839c94f01f7e1e3d58fdc6901e
April Huang <phuang@beaconfireinc.com>
2023-03-01 11:12:48 -0500

    Initial commit

---
+++ src/main/java/com/example/workflowdemo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.workflowdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping(value = {"/", "/demo", "/another-demo"}) // match both "/" and "/demo"
+    public String demo() {
+        return "demo works!";
+    }
+
+}
---
+++ src/test/java/com/example/workflowdemo/controller/DemoControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.workflowdemo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoControllerTest {
+
+    @Test
+    void testItem1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void testItem2() {
+        assertEquals(2, 2);
+    }
+}
